147ce8f9394ffa5afdd7a97743440feea1faa085
Italo-Tech <italosantosdick@hotmail.com>
2020-03-26 16:37:40 -0300

    Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+// JSX (JavaScript XML)- html integrado com javascript
+function App() {  // Função que retorna o HTML
+  return (
+    <Routes />
+  );
+
+}
+
+export default App;
